793d6c4dad1259a7d60ad3df3d027d8895a55c7d
zhang_cn <17310408750@163.com>
2019-04-16 16:21:14 +0800

    生成项目

---
+++ src/navigator/AppNavigators.js
@@ -0,0 +1,12 @@
+/**
+ * @private 此文件是App的导航文件
+ * @param 包含了底部导航栏和顶部导航栏
+ */
+
+import {
+  createStackNavigator,
+  createSwitchNavigator,
+  createMaterialTopTabNavigator,
+  createBottomTabNavigator,
+  createAppContainer
+} from "react-navigation"
